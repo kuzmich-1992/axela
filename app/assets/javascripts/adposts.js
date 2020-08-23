e2d47d5c66079280a39dd667164bb5111d933ab3
Dima <kuzmichman92@gmail.com>
2020-08-23 14:34:50 +0300

    process of utgragestring to json ajax

--- app/assets/javascripts/adposts.js
+++ app/assets/javascripts/adposts.js
@@ -3,38 +3,31 @@ $(document).ready(function(){
   $('#submitadv').on('click',function(event) {
   event.preventDefault();
 
-    var dataString = $('#adpost-form-group').serialize();
-
-    var dataJson = JSON.stringify(dataString);
-
-    // var price = jQuery("#price").val();
-    // var year = jQuery("#year").val();
-    // var model = jQuery("#model").val();
-    // var serie = jQuery("#serie").val();
-    // var body  = jQuery("#body").val(); 
-    // var gearbox  = jQuery("#gearbox").val(); 
-    // var engine_type  = jQuery("#engine_type").val(); 
-    // var location  = jQuery("#location").val(); 
-    // var adpost_image = jQuery("#adpost_image").val(); 
-        
-    // var dataString = 'price='+ price + '&year=' + year + '&model=' + model + '&serie=' + serie + 
-    //                  '&body=' + body + '&gearbox=' + gearbox + '&engine_type=' + engine_type +
-    //                  '&location=' + location + '&adpost_image=' + adpost_image; 
+    var dataString = $('#adpost-form-group')
+    
+    var price = jQuery("#price").val();
+    var year = jQuery("#year").val();
+    var model = jQuery("#model").val();
+    var serie = jQuery("#serie").val();
+    var body  = jQuery("#body").val(); 
+    var gearbox  = jQuery("#gearbox").val(); 
+    var engine_type  = jQuery("#engine_type").val(); 
+    var location  = jQuery("#location").val(); 
+    var adpost_image = jQuery("#adpost_image").val(); 
     
     $.ajax({
       method: "POST",  
       url: '/adposts',
-      data: 'adpost':{dataJson} ,
-      contentType: "application/json; charset=utf-8",
-      datatype: 'json',
+      data:{ adpost : dataString },
+      dataType: 'json',
     }).done(function(data){
-
+    
       console.log('success');
 
     }).fail(function() {
       
       console.log('fail');
-      
+
     });
     
     });
--- app/controllers/adposts_controller.rb
+++ app/controllers/adposts_controller.rb
@@ -1,5 +1,7 @@
 class AdpostsController < ApplicationController
+  require 'pry'
   skip_before_action :verify_authenticity_token
+  respond_to :json
   
   def index
     @adposts = Adpost.includes(:user) # without N+1 query
@@ -8,12 +10,12 @@ class AdpostsController < ApplicationController
   def new
     @adpost = Adpost.new(adposts_params)
   end
-
+ 
   def create
     @adpost = current_user.adposts.build(adposts_params)
     if @adpost.save!
       respond_to do |format|
-        format.json { head :no_content }
+        format.json { render json: @adpost }
         format.html { redirect_to request.referrer }
       end
     end
@@ -28,7 +30,7 @@ class AdpostsController < ApplicationController
   def show
     @adpost = Adpost.find(params[:id])
   end
-  
+
   def adposts_params
   	params.require(:adpost).permit(:price, :year, :model, :serie, :body, :gearbox, :engine_type, :location, :adpost_image)
   end
