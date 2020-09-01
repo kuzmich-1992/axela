c79fab3406af69a817e8362d79626037fdd9247c
Dima <kuzmichman92@gmail.com>
2020-09-01 14:03:41 +0300

    multipart process1

--- app/assets/javascripts/adposts.js
+++ app/assets/javascripts/adposts.js
@@ -1,41 +1,68 @@
-$(document).ready(function () {
-  $("#btnSubmit").click(function (event) {
-    event.preventDefault();
-      var form = $('#carform')[0];
-      var data = new FormData(form);
-      data.append("CustomField", "This is some extra data, testing");
-      $("#btnSubmit").prop("disabled", true);
+// $(document).ready(function () {
+//   $("#btnSubmit").click(function (event) {
+//     event.preventDefault();
+//       var form = $('#carform')[0];
+//       var data = new FormData(form)
 
-      $.ajax({
-            type: "POST",
-            enctype: 'multipart/form-data',
-            url: "/adposts",
-            data: data,
-            processData: false,
+//       $.ajax({
+//             type: "POST",
+//             enctype: 'multipart/form-data',
+//             url: "/adposts",
+//             data: data,
+//             processData: false,
+//             contentType: false,
+//             cache: false,
+//             timeout: 600000,
+//             success: function (data) {
+
+//                 $("#result").text(data);
+//                 console.log("SUCCESS : ", data);
+//                 $("#btnSubmit").prop("disabled", false);
+
+//             },
+//             error: function (e) {
+
+//                 $("#result").text(e.responseText);
+//                 console.log("ERROR : ", e);
+//                 $("#btnSubmit").prop("disabled", false);
+
+//             }
+//         });
+
+//     });
+
+// });
+
+
+
+$(document).ready(function(){
+
+    $("#btnSubmit").click(function(){
+
+        var fd = new FormData();
+        var files = $('#file')[0].files[0];
+        var form = $('#carform')[0];
+        fd.append('file',files);
+        fd.append('carform');
+
+        $.ajax({
+            url: '/adposts',
+            type: 'post',
+            dataType: 'json',
+            data: fd,
             contentType: false,
-            cache: false,
-            timeout: 600000,
+            processData: false,
             success: function (data) {
 
-                $("#result").text(data);
+                
                 console.log("SUCCESS : ", data);
-                $("#btnSubmit").prop("disabled", false);
-
+               
             },
             error: function (e) {
 
-                $("#result").text(e.responseText);
                 console.log("ERROR : ", e);
-                $("#btnSubmit").prop("disabled", false);
-
-            }
+              
+            },
         });
-
     });
-
-});
-
-
-
-
- 
+}); 
