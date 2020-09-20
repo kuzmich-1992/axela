cff4f653f283e67938cab5c5dbdcca0948050aa0
Dima <kuzmichman92@gmail.com>
2020-09-20 12:30:38 +0300

    add rented car as json

---
+++ app/assets/javascripts/rented_cars.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $('#submitcar').on('click',function(event) {
+  event.preventDefault();
+
+  var fd = new FormData(document.querySelector("form"));
+  fd.append("CustomField", "This is some extra data");
+
+  $.ajax({
+    url: '/rented_cars',
+    type: "POST",
+    data: fd,
+    dataType: 'json',
+    cache: false,
+    processData: false,  // Сообщить jQuery не передавать эти данные
+    contentType: false   // Сообщить jQuery не передавать тип контента
+    }).done(function (data){
+      $("#rented_cars").load(location.href+" #rented_cars>*","");
+    });  
+  
+  });
+
+});
