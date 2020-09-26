f5654cf04593b2bf6018392930f8561262d1ff61
Dima <kuzmichman92@gmail.com>
2020-09-27 02:16:12 +0300

    leases as json

---
+++ app/assets/javascripts/rent_leases.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+  $('#submitrent').on('click',function(event) {
+  event.preventDefault();
+
+  var fd = new FormData(document.querySelector("#new_rented_lease"));
+  fd.append("CustomField", "This is some extra data");
+
+  $.ajax({
+    url: '/rented_leases',
+    type: "POST",
+    data: fd,
+    dataType: 'json',
+    cache: false,
+    processData: false,  // Сообщить jQuery не передавать эти данные
+    contentType: false   // Сообщить jQuery не передавать тип контента
+    })
+  
+  });
+
+});
