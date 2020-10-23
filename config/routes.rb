0155374890ece396430cd8e878d08a30d4987481
Dima <kuzmichman92@gmail.com>
2020-10-23 06:13:13 +0300

    root adposr

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :chat_messages
   resources :conversations
   resources :messages
+  root 'adposts#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
