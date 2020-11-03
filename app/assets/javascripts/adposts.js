0820f8ceef9f27ad6eb54d8cc46a627627be0c9b
Dima <kuzmichman92@gmail.com>
2020-11-03 10:26:21 +0200

    add images to clud from form

--- app/assets/javascripts/adposts.js
+++ app/assets/javascripts/adposts.js
@@ -1,24 +1,24 @@
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  $('#submitadv').on('click',function(event) {
-  event.preventDefault();
+//   $('#submitadv').on('click',function(event) {
+//   event.preventDefault();
 
-var fd = new FormData(document.querySelector("form"));
-fd.append("CustomField", "This is some extra data");
+// var fd = new FormData(document.querySelector("form"));
+// fd.append("CustomField", "This is some extra data");
 
-  $.ajax({
-    url: '/adposts',
-    type: "POST",
-    data: fd,
-    dataType: 'json',
-    cache: false,
-    processData: false,  // Сообщить jQuery не передавать эти данные
-    contentType: false   // Сообщить jQuery не передавать тип контента
-    }).done(function (data){
-      $("#adposts").load(location.href+" #adposts>*","");
-    });
+//   $.ajax({
+//     url: '/adposts',
+//     type: "POST",
+//     data: fd,
+//     dataType: 'json',
+//     cache: false,
+//     processData: false,  // Сообщить jQuery не передавать эти данные
+//     contentType: false   // Сообщить jQuery не передавать тип контента
+//     }).done(function (data){
+//       $("#adposts").load(location.href+" #adposts>*","");
+//     });
 
-  });
+//   });
 
-});
+// });
