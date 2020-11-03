60f190e2acde5518aadb6d5fe859942fa413fc33
Dima <kuzmichman92@gmail.com>
2020-11-03 11:12:22 +0200

    cloud ok2

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'kuzmich-1992'
-  config.api_key = '655944766768318'
-  config.api_secret = 'DAUpO-7xIRx2ZHzBQR5B-NPlTSE'
-  config.secure = true
-  config.cdn_subdomain = true
-end
